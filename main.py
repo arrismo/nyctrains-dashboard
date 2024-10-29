d94372b0d87e55bf5afa4b6de17dd7ce610d9976
arrismo <mikaelmoise00@gmail.com>
2024-10-29 19:18:57 -0400

    saving

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from google.transit import gtfs_realtime_pb2
 from protobuf3_to_dict import protobuf_to_dict
 
 # Get MTA ACE Train Data
-# MTA train data is returen in a Protocol Buffer format (GTFS format)
+# MTA train data is returned in a Protocol Buffer format (GTFS format)
 # response = requests.get("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace")
 # feed = gtfs_realtime_pb2.FeedMessage()
 # feed.ParseFromString(response.content)
